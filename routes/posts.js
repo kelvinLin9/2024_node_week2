d7767a1485430de2b13f6a4dbd9aa98114ea65cc
kelvin <kelvin80121@gmail.com>
2024-04-27 16:11:55 +0800

    Refactor routes in posts.js to use postController

--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,18 @@
-import post
+import postController from '../controllers/posts.js';
 
 const routes = async (req, res) => {
   const { url, method } = req;
   if (url === '/posts' && method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'GET posts' }));
+    postController.getPosts(req, res);
   } else if (url === '/posts' && method === 'POST') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'POST posts' }));
+    postController.createPost(req, res);
+  } else if (url === '/posts/:id' && method === 'PATCH') {
+    postController.updatePost(req, res);
+  } else if (url === '/posts/:id' && method === 'DELETE') {
+    postController.deletePost(req, res);
   } else {
     res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Not Found' }));
+    res.end(JSON.stringify({ message: 'Route not found' }));
   }
 }
+export default routes;
